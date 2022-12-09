7502d9be06f87bd59856f7096dfcf7f3d5a71984
mrroshan064 <roshanmahato351@gmil.com>
2022-12-10 00:07:50 +0545

    lang with i18n

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "./utils/i18n";
+
+ReactDOM.render(
+  <Suspense fallback="loading">
+    <App />
+  </Suspense>,
+  document.getElementById("root")
+);
